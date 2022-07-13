87322f29e9b11fb3b9f1d04d8e0e7fa3a185ac6a
slocksert <rsdlj01@gmail.com>
2022-07-13 17:03:34 -0300

    updating content and manifest

--- Content.js
+++ Content.js
@@ -2,11 +2,9 @@ const Element = '.ScCoreButtonSuccess-sc-1qn4ixc-5'
 
 function clickButton()
 {
-    if (!!document.querySelector(Element) == 'true'){
-        document.querySelector(Element).click() 
-    } else {
-        //pass
-        console.log("Button doesn't exist yet")
+    let button = document.querySelector(Element)
+    if (button){
+        button.click()
     }
 }
 
